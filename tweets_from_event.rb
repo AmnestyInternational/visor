75311683771593591b95053fa99e0edf8c486661
gregology <greg.clarke@jadedpixel.com>
2014-06-28 15:56:30 -0400

    working with users

--- tweets_from_event.rb
+++ tweets_from_event.rb
@@ -6,8 +6,6 @@ def fetch_tweets_from_event(event, since_id = 0)
   @client.search( "", parameters ).to_h
 end
 
-sql = ""
-
 fetch_tweets_from_event(event)[:statuses].each do |tweet|
   sql = """
     REPLACE INTO tweets (tweet_id, user_id, text, created_at, retweet_count, favorite_count)
@@ -18,6 +16,27 @@ fetch_tweets_from_event(event)[:statuses].each do |tweet|
       '#{DateTime.parse(tweet[:created_at]).strftime("%Y-%m-%d %H:%M")}',
       '#{tweet[:retweet_count]}',
       '#{tweet[:favorite_count]}');\n"""
+
+  puts sql
+  
+  @db.query(sql)
+
+  sql = """
+    REPLACE INTO twitter_users (user_id, name, screen_name, created_at, location, description, url, followers_count, friends_count, listed_count, favourites_count, statuses_count, utc_offset)
+    VALUES (
+      '#{tweet[:user][:id]}',
+      '#{Mysql.escape_string(tweet[:user][:name].to_s)}',
+      '#{Mysql.escape_string(tweet[:user][:screen_name].to_s)}',
+      '#{DateTime.parse(tweet[:user][:created_at]).strftime("%Y-%m-%d %H:%M")}',
+      '#{Mysql.escape_string(tweet[:user][:location].to_s)}',
+      '#{Mysql.escape_string(tweet[:user][:description].to_s)}',
+      '#{Mysql.escape_string(tweet[:user][:url].to_s)}',
+      '#{tweet[:user][:followers_count]}',
+      '#{tweet[:user][:friends_count]}',
+      '#{tweet[:user][:listed_count]}',
+      '#{tweet[:user][:favourites_count]}',
+      '#{tweet[:user][:statuses_count]}',
+      '#{tweet[:user][:utc_offset]}');\n"""
 
   puts sql
 
